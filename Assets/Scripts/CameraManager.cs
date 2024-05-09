135f17c4684a19495decb12e9b6ab894af463057
GalteXx <maximkors@gmail.com>
2024-05-09 17:26:03 +0300

    feat(player): added camera,camera api

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,27 @@
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraManager : MonoBehaviour
+{
+
+    [SerializeField] private VolumeProfile volumeProfile;
+    [SerializeField] private CinemachineVirtualCamera virutalCamera;
+
+    private CinemachineImpulseSource _impulseSource;
+
+    public VolumeProfile VolumeProfile { get => volumeProfile; }
+    public CinemachineImpulseSource ImpulseSource { get => _impulseSource; }
+
+    public float CameraZoom { get => virutalCamera.m_Lens.FieldOfView; set => virutalCamera.m_Lens.FieldOfView = value; }
+
+    private void Start()
+    {
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    public void ShakeCamera()
+    {
+        _impulseSource.GenerateImpulse();
+    }
+}
