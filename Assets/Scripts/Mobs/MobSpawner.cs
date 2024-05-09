2deade8507f2378dd4982c69ea1e9695a18d4e21
Mikov Denis <denis.mikov.06@gmail.com>
2024-05-09 18:15:17 +0300

    feat(mob): implemented mob spawner

---
+++ Assets/Scripts/Mobs/MobSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace SGJ.Mobs 
+{ 
+    public class MobSpawner : MonoBehaviour
+    {
+        private const string MOBS_PATH = "Prefabs/Mobs";
+        [SerializeField] private int mobsQuantity;
+
+        private PlayerMovement _player;
+        private Mob[] _mobs;
+
+        private void Start()
+        {
+            _player = FindObjectOfType<PlayerMovement>();
+            _mobs = Resources.LoadAll<Mob>(MOBS_PATH);
+            SpawnMobs();
+        }
+
+        private void SpawnMobs()
+        {
+            for (int i = 0; i < mobsQuantity; i++)
+            {
+                foreach (var mob in _mobs)
+                {
+                    var instance = Instantiate(mob, transform);
+                    instance.SetMobParameters(_player.transform);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            #if UNITY_EDITOR
+            
+            // Debug ONLY
+            if (Input.GetKeyDown(KeyCode.Space))
+                SpawnMobs();
+            
+            #endif
+        }
+
+    }
+}
