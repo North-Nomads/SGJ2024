2099a1378983d1c085bc1bbd1f799ca43c87c1f5
Mikov Denis <denis.mikov.06@gmail.com>
2024-05-11 11:52:44 +0300

    feat(jumper): added charge animation

--- Assets/Scripts/Mobs/Jumper/JumperAttackState.cs
+++ Assets/Scripts/Mobs/Jumper/JumperAttackState.cs
@@ -7,29 +7,41 @@ namespace SGJ.Mobs.Saw
     public class JumperAttackState : MobState
     {
         private readonly AnimationCurve _jumpHeightCurve;
+        private readonly Transform _jumperHead;
         private readonly float _jumpChargeTime;
         private readonly float _jumpSpeed;
         private readonly float _startJumpRange;
         private readonly float _jumpDuration;
+        private readonly float _defaultPositionY;
+        private readonly float _desiredPositionY;
+        private readonly Transform _jumperLegs;
+        private readonly Vector3 _defaultLegsScale;
+        private readonly Vector3 _desiredLegsScale;
         private float _jumpChargeTimeLeft;
         private bool _isInJump;
 
         public JumperAttackState(NavMeshAgent agent, JumpMob jumpMob, Transform player, float startJumpRange, float jumpDuration,
-            float jumpChargeTime, AnimationCurve jumpHeightCurve, float jumpSpeed) 
-            : base(agent, jumpMob, player)
+            float jumpChargeTime, AnimationCurve jumpHeightCurve, float jumpSpeed, Transform head, float defaultPositionY, 
+            float desiredPositionY, Transform jumperLegs, Vector3 defaultScale, Vector3 desiredScale) : base(agent, jumpMob, player)
         {
             _startJumpRange = startJumpRange;
             _jumpDuration = jumpDuration;
             _jumpChargeTime = jumpChargeTime;
             _jumpHeightCurve = jumpHeightCurve;
             _jumpSpeed = jumpSpeed;
+            _jumperHead = head;
+            _defaultPositionY = defaultPositionY;
+            _desiredPositionY = desiredPositionY;
+            _jumperLegs = jumperLegs;
+            _defaultLegsScale = defaultScale;
+            _desiredLegsScale = desiredScale;
         }
 
         public override void BehaveThisState()
         {
             if (_isInJump)
             {
-                Transform.LookAt(Player.position);
+                Agent.SetDestination(Player.position);
                 return;   
             }
 
@@ -45,8 +57,23 @@ namespace SGJ.Mobs.Saw
             IEnumerator PerformJumpAfterDelay()
             {
                 _isInJump = true;
-                yield return new WaitForSeconds(_jumpChargeTime);
+                var chargingElapsedTime = 0f;
+                var percent = chargingElapsedTime / _jumpChargeTime;
+                while (chargingElapsedTime < _jumpChargeTime)
+                {
+                    _jumperHead.position = new Vector3(_jumperHead.position.x,
+                        Mathf.Lerp(_defaultPositionY, _desiredPositionY, percent),
+                        _jumperHead.position.z);
+                    
+                    _jumperLegs.localScale = Vector3.Lerp(_defaultLegsScale, _desiredLegsScale, percent);
 
+                    chargingElapsedTime += Time.deltaTime;
+                    percent = chargingElapsedTime / _jumpChargeTime;
+                    yield return null;
+                }
+
+                _jumperHead.position = new Vector3(_jumperHead.position.x, _defaultPositionY, _jumperHead.position.z);
+                _jumperLegs.localScale = _defaultLegsScale;
                 Agent.speed = _jumpSpeed;
                 Agent.SetDestination(Player.position);
 
@@ -67,14 +94,8 @@ namespace SGJ.Mobs.Saw
             }
         }
 
-        public override void OnStateStarted()
-        {
-            Agent.speed = 0f;
-        }
+        public override void OnStateStarted() => Agent.speed = 0f;
 
-        public override void OnStateStopped()
-        {
-            Agent.speed = Self.DefaultSpeed;
-        }
+        public override void OnStateStopped() => Agent.speed = Self.DefaultSpeed;
     }
 }
