1505c8d3ae44cda7ed1ccd4549072bd9d975671d
Mikov Denis <denis.mikov.06@gmail.com>
2024-05-11 12:48:59 +0300

    refactor(hatch): removed sonar and analyser phrases

--- Assets/Scripts/SceneManagement/NextLevelHatch.cs
+++ Assets/Scripts/SceneManagement/NextLevelHatch.cs
@@ -1,5 +1,4 @@
-﻿using SGJ.GameItems;
-using SGJ.Player;
+﻿using SGJ.Player;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +8,9 @@ namespace SGJ.SceneManagement
     public enum LevelDifficulty
     {
         Peace = 0,
-        Easy = 3,
-        Medium = 6,
-        Hard = 8
+        Easy = 2,
+        Medium = 4,
+        Hard = 6
     }
 
     public class NextLevelHatch : MonoBehaviour
@@ -29,28 +28,11 @@ namespace SGJ.SceneManagement
         public string GetHatchInfo => $"Угроза: {_analysisInfo}\nАнализ форм жизней: {_sonarInfo}";
         public EventHandler<LevelDifficulty> OnHatchTriggered = delegate { };
         
-        private Dictionary<LevelDifficulty, string> _sonarPhrases;
-        private Dictionary<LevelDifficulty, string> _lifeFormPhrases;
 
         private void Start()
         {
             if (!isHubHatch)
                 _animator = GetComponentInChildren<Animator>();
-
-            _sonarPhrases = new Dictionary<LevelDifficulty, string>
-            {
-                { LevelDifficulty.Peace, "Не обнаружена" },
-                { LevelDifficulty.Easy, "Обнаружена" },
-                { LevelDifficulty.Medium, "Умеренная" },
-                { LevelDifficulty.Hard, "Высокая активность" }
-            };
-            _lifeFormPhrases = new Dictionary<LevelDifficulty, string>
-            {
-                { LevelDifficulty.Peace, "Не проведен" },
-                { LevelDifficulty.Easy, "<50" },
-                { LevelDifficulty.Medium, ">100" },
-                { LevelDifficulty.Hard, ">300" }
-            };
         }
 
         private void OnTriggerEnter(Collider other)
